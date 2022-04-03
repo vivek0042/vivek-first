9953a7f34a09f8394f688c45f7d5d9f09da8ea5b
vivek jain <100334280+vivek0042@users.noreply.github.com>
2022-04-03 08:43:49 +0530

    my calculator in python

---
+++ calc.py
@@ -0,0 +1,13 @@
+
+def do_operation(X, Y):
+   X=raw_input("enter value")
+   Y=raw_input("enter value")
+   print(X+Y) 
+   print(X-Y)
+   print(X*Y)
+   print(X/Y)
+   print(X**Y)
+   print(X//Y)
+   return (0)
+    
+    
